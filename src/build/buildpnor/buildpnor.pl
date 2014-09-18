d0c872fdfa0516fa440e32e4208424b54df77a33
Dean Sanner <dsanner@us.ibm.com>
2014-09-18 15:00:23 -0500

    Update PNOR build script to handle two sides
    Change-Id: I61db4d83ba8ea7ddc61c776625b43a07ee0039b0
    RTC: 115870

--- src/build/buildpnor/buildpnor.pl
+++ src/build/buildpnor/buildpnor.pl
@@ -6,7 +6,9 @@
 #
 # OpenPOWER HostBoot Project
 #
-# COPYRIGHT International Business Machines Corp. 2012,2014
+# Contributors Listed Below - COPYRIGHT 2012,2014
+# [+] International Business Machines Corp.
+#
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -165,17 +167,17 @@ if($rc != 0)
 my $sideAOffset =  $pnorLayout{metadata}{sideAOffset};
 my $sideBOffset =  $pnorLayout{metadata}{sideBOffset};
 
-$rc = createPnorImage($tocVersion, $pnorBinName, \%pnorLayout, $sideAOffset);
+$rc = createPnorImg($tocVersion, $pnorBinName, \%pnorLayout, $sideAOffset, 'A');
 if($rc != 0)
 {
-    trace(0, "Error detected from call to createPnorImage() sideAOffset. Exiting");
+    trace(0, "Error detected from createPnorImg() sideAOffset. Exiting");
     exit 1;
 }
 
-$rc = createPnorImage($tocVersion, $pnorBinName, \%pnorLayout, $sideBOffset);
+$rc = createPnorImg($tocVersion, $pnorBinName, \%pnorLayout, $sideBOffset, 'B');
 if($rc != 0)
 {
-    trace(0, "Error detected from call to createPnorImage() sideBOffset. Exiting");
+    trace(0, "Error detected from createPnorImg() sideBOffset. Exiting");
     exit 1;
 }
 
@@ -270,17 +272,23 @@ sub loadPnorLayout
 }
 
 ################################################################################
-# createPnorImage - Create PNOR image and partitions based on input data.
+# createPnorImg - Create PNOR image and partitions based on input data.
 ################################################################################
-sub createPnorImage
+sub createPnorImg
 {
-    my ($i_tocVersion, $i_pnorBinName, $i_pnorLayoutRef, $offset) = @_;
+    my ($i_tocVersion, $i_pnorBinName, $i_pnorLayoutRef, $offset, $side) = @_;
     my $this_func = (caller(0))[3];
     my $rc = 0;
     my $key;
+    my $other_side = 'B';
     trace(4, "$this_func: >>Enter");
 
-    trace(1, "createPnorImage:: $offset");
+    trace(1, "createPnorImg:: $offset");
+
+    if($side eq 'B')
+    {
+        $other_side = 'A';
+    }
 
     #get Block size
     my $blockSize = $$i_pnorLayoutRef{metadata}{blockSize};
@@ -322,9 +330,11 @@ sub createPnorImage
 
         # eyecatcher
         my $eyeCatch = $sectionHash{$key}{eyeCatch};
+        my $myside = $sectionHash{$key}{side};
 
-        #don't try to add the eyeCatcher, but need to update user data
-        if( $eyeCatch ne $g_TOCEyeCatch )
+        #don't try to add the TOC, but need to update all other paritions
+        #Add if side matches (or not set) -- so if it isn't equal to other side
+        if(( $eyeCatch ne $g_TOCEyeCatch ) && ( $myside ne $other_side ))
         {
 
             # base/physical offset
